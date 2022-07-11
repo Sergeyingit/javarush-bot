9320f1431f24870f9c07d9f9baceba23cadc7d66
Sergey <sergeyakkaynt@gmail.com>
2022-07-11 19:19:25 +0300

    added spring boot to project

---
+++ src/main/java/com/github/sergeyingit/javarushbot/JavarushBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.sergeyingit.javarushbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarushBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarushBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sergeyingit/javarushbot/JavarushBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sergeyingit.javarushbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
