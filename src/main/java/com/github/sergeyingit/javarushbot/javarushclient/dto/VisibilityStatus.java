21f4231b388f95f04037d56f3ca9f877e21ef011
Sergey <sergeyakkaynt@gmail.com>
2022-07-23 21:11:53 +0300

    STEP_8_JRTB-4: *   JRTB-4: added ability to send notifications about new articles *   JRTB-8: added ability to set inactive telegram user *   JRTB-9: added ability to set active user and/or start using it.

---
+++ src/main/java/com/github/sergeyingit/javarushbot/javarushclient/dto/VisibilityStatus.java
@@ -0,0 +1,14 @@
+package com.github.sergeyingit.javarushbot.javarushclient.dto;
+
+/**
+ * DTO, which represents visibility status.
+ */
+public enum VisibilityStatus {
+    UNKNOWN,
+    RESTRICTED,
+    PUBLIC,
+    PROTECTED,
+    PRIVATE,
+    DISABLED,
+    DELETED;
+}
---
+++ src/main/java/com/github/sergeyingit/javarushbot/service/FindNewArticleService.java
@@ -0,0 +1,11 @@
+package com.github.sergeyingit.javarushbot.service;
+
+/**
+ * Service for finding new articles.
+ */
+public interface FindNewArticleService {
+    /**
+     * Find new articles and notify subscribers about it.
+     */
+    void findNewArticles();
+}
